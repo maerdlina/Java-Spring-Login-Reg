7144f2d091fcbc110869eb59c64182557d25053a
maerdlina <58111252+maerdlina@users.noreply.github.com>
2024-05-26 17:02:27 +0300

    axios

--- frontend/src/components/AppContent.js
+++ frontend/src/components/AppContent.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import WelcomeContent from './WelcomeContent';
+import AuthContents from './AuthContent';
 
 export default class AppContent extends React.Component{
     render(){
         return(
             <div>
                 <WelcomeContent/>
+                <AuthContents/>
             </div>
         );
     };
